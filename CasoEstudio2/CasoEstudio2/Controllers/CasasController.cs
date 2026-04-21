f8d3ef99b86993ac4274d386363a412997206d6e
Rafael Solano S. <rafaelsolanosilv@gmail.com>
2026-04-21 11:23:01 -0600

    Caso_2

---
+++ CasoEstudio2/CasoEstudio2/Controllers/CasasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CasoEstudio2.Controllers
+{
+    [Route("casas/[controller]")]
+    public class CasasController : Controller
+    {
+        [HttpGet("ConsultaCasas")]
+        public IActionResult ConsultarCasas()
+        {
+            return View();
+        }
+
+        [HttpGet("AlquilarCasas")]
+        public IActionResult AlquilarCasas()
+        {
+            return View();
+        }
+    }
+}
---
+++ CasoEstudio2/CasoEstudio2/Models/CasasModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+namespace CasoEstudio2.Models
+{
+    public class CasasModel
+    {
+        public long IdCasa { get; set; }
+        public string DescripcionCasa { get; set; }
+        public decimal PrecioCasa { get; set; }
+        public string? UsuarioAlquiler { get; set; }
+        public DateTime? FechaAlquiler { get; set; }
+
+    }
+}
